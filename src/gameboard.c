330e7c44edef030399525d87a5d24a480ee9e9f3
Jaeheon Shim <git@jaeheonshim.com>
2021-08-22 10:52:31 -0400

    Add all pointer rectangles

--- src/gameboard.c
+++ src/gameboard.c
@@ -5,18 +5,29 @@ static Rectangle spot0;
 static float scale;
 static Vector2 position;
 
+static Rectangle spots[9];
+
 static void UpdateRectangles() {
-    spot0.x = position.x;
-    spot0.y = position.y;
-    spot0.height = background.width * scale * 0.33;
-    spot0.width = background.width * scale * 0.33;
+    float width = background.width * scale * 0.33;
+    for(int i = 0; i < 9; i++) {
+        spots[i].x = position.x + (i % 3) * width;
+        spots[i].y = position.y + (i / 3) * width;
+        spots[i].height = width;
+        spots[i].width = width;
+    }
 }
 
 static void UpdateCursor() {
     Vector2 mousePosition = GetMousePosition();
-    if(CheckCollisionPointRec(mousePosition, spot0)) {
-        SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
-    } else {
+    int i;
+    for(i = 0; i < 9; i++) {
+        if(CheckCollisionPointRec(mousePosition, spots[i])) {
+            SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+            break;
+        }
+    }
+
+    if(i >= 9) {
         SetMouseCursor(MOUSE_CURSOR_DEFAULT);
     }
 }
@@ -27,7 +38,7 @@ void InitGameBoard() {
 
 void UpdateGameBoard() {
     scale = (GetScreenWidth() / (float) background.width) * 0.5;
-    
+
     position.x = GetScreenWidth() / 2 - (background.width * scale) / 2;
     position.y = GetScreenHeight() / 2 - (background.height * scale) / 2;
 
@@ -37,5 +48,8 @@ void UpdateGameBoard() {
 
 void DrawGameBoard() {
     DrawTextureEx(background, position, 0, scale, WHITE);
-    DrawRectangleRec(spot0, RED);
+
+    for(int i = 0; i < 9; i++) {
+        DrawRectangleRec(spots[i], BLUE);
+    }
 }
