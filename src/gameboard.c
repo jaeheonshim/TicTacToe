d6ed78b2bc5a4735b6700cd919321dd2ccc69856
Jaeheon Shim <git@jaeheonshim.com>
2021-08-22 20:02:39 -0400

    Refactor texture names

--- src/gameboard.c
+++ src/gameboard.c
@@ -5,9 +5,9 @@
 #define NORMAL 0
 #define TEXTURE 1
 
-static Texture2D background;
-static Texture2D x;
-static Texture2D o;
+static Texture2D t_background;
+static Texture2D t_x;
+static Texture2D t_o;
 
 static Rectangle spot0;
 static float scale;
@@ -19,7 +19,7 @@ static char cursorState = NORMAL;
 static float cursorScale = 0.20;
 
 static void UpdateRectangles() {
-    float width = background.width * scale * 0.33;
+    float width = t_background.width * scale * 0.33;
     for(int i = 0; i < 9; i++) {
         spots[i].x = position.x + (i % 3) * width;
         spots[i].y = position.y + (i / 3) * width;
@@ -60,23 +60,23 @@ static void HandleClick() {
 static void DrawCursorTexture() {
     Vector2 mousePosition = GetMousePosition();
     if(GameToPlay() == TOKEN_X) {
-        DrawTextureEx(x, Vector2(mousePosition.x - x.width * cursorScale / 2, mousePosition.y - x.height * cursorScale / 2), 0, cursorScale, WHITE);
+        DrawTextureEx(t_x, Vector2(mousePosition.x - t_x.width * cursorScale / 2, mousePosition.y - t_x.height * cursorScale / 2), 0, cursorScale, WHITE);
     } else if(GameToPlay() == TOKEN_O) {
-        DrawTextureEx(o, Vector2(mousePosition.x - o.width * cursorScale / 2, mousePosition.y - o.height * cursorScale / 2), 0, cursorScale, WHITE);
+        DrawTextureEx(t_o, Vector2(mousePosition.x - t_o.width * cursorScale / 2, mousePosition.y - t_o.height * cursorScale / 2), 0, cursorScale, WHITE);
     }
 }
 
 void InitGameBoard() {
-    background = LoadTexture("assets/background.png");
-    x = LoadTexture("assets/x.png");
-    o = LoadTexture("assets/o.png");
+    t_background = LoadTexture("assets/background.png");
+    t_x = LoadTexture("assets/x.png");
+    t_o = LoadTexture("assets/o.png");
 }
 
 void UpdateGameBoard() {
-    scale = (GetScreenWidth() / (float) background.width) * 0.5;
+    scale = (GetScreenWidth() / (float) t_background.width) * 0.5;
 
-    position.x = GetScreenWidth() / 2 - (background.width * scale) / 2;
-    position.y = GetScreenHeight() / 2 - (background.height * scale) / 2;
+    position.x = GetScreenWidth() / 2 - (t_background.width * scale) / 2;
+    position.y = GetScreenHeight() / 2 - (t_background.height * scale) / 2;
 
     UpdateRectangles();
     UpdateCursor();
@@ -87,14 +87,14 @@ void UpdateGameBoard() {
 }
 
 void DrawGameBoard() {
-    float width = background.width * scale * 0.3333;
-    DrawTextureEx(background, position, 0, scale, WHITE);
+    float width = t_background.width * scale * 0.3333;
+    DrawTextureEx(t_background, position, 0, scale, WHITE);
 
     for(int i = 0; i < 9; i++) {
         if(GameGetToken(i) == TOKEN_O) {
-            DrawTextureEx(o, Vector2((position.x + width * (i % 3)) + (width - o.width * scale) / 2, (position.y + width * (i / 3)) + (width - o.height * scale) / 2), 0, scale, WHITE);
+            DrawTextureEx(t_o, Vector2((position.x + width * (i % 3)) + (width - t_o.width * scale) / 2, (position.y + width * (i / 3)) + (width - t_o.height * scale) / 2), 0, scale, WHITE);
         } else if(GameGetToken(i) == TOKEN_X) {
-            DrawTextureEx(x, Vector2((position.x + width * (i % 3)) + (width - x.width * scale) / 2, (position.y + width * (i / 3)) + (width - x.height * scale) / 2), 0, scale, WHITE);
+            DrawTextureEx(t_x, Vector2((position.x + width * (i % 3)) + (width - t_x.width * scale) / 2, (position.y + width * (i / 3)) + (width - t_x.height * scale) / 2), 0, scale, WHITE);
         }
     }
 
